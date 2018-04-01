aee897c9425992f86109488caa4fefeef73c8e20
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-04-01 22:07:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function placeOrder(cardNumber) {
   if (!cardNumber){
     return "Sorry, we don't have a credit card on file for you."
   }else{
-    money = total()
+    var money = total()
     setCart([])
     return `Your total cost is $${money}, which will be charged to the card ${cardNumber}.`
   }
