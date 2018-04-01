af596dcbe0f078f11c25ebc7196c4080f2a1e820
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-04-01 21:00:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function total() {
 function removeFromCart(item) {
   for (let i = 0 ;i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      cart.splice [i,1]
+      cart.splice[i,1]
     }
   } return cart
 }
