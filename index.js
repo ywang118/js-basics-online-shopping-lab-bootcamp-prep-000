4f79cba2e67648bfb66df74f31f7e759dcbb1d4f
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-04-01 22:01:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,5 +63,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (!cardNumber){
     return "Sorry, we don't have a credit card on file for you."
+  }else{
+    return ``
   }
 }
